6523d730f4262ea818e80163c36eba94697a55f1
nevstafev <evstafev.n.e@gmail.com>
2017-06-07 02:02:53 +0300

    Comment unused components.

--- src/Products/Show/Header.jsx
+++ src/Products/Show/Header.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import media from './../../utils/media';
-import Label from '../../components/Label';
+// import Label from '../../components/Label';
 
 const Wrapper = styled.header`
   flex: 0 0 100%;
@@ -47,27 +47,27 @@ const Name = styled.h1`
   `}
 `;
 
-const Save = styled.button`
-  display: none;
-  height: 75px;
-  width: 75px;
-  color: #ffffff;
-  font-size: 18px;
-  font-weight: bold;
-  text-transform: uppercase;
-  border-radius: 50%;
-  background-color: ${props => props.color};
-  position: absolute;
-  top: 200px;
-  left: 28px;
-  cursor: pointer;
-  text-transform: uppercase;
-  border: none;
-  outline: none;
-  ${media.small`
-    display: inline;
-  `}
-`;
+// const Save = styled.button`
+//   display: none;
+//   height: 75px;
+//   width: 75px;
+//   color: #ffffff;
+//   font-size: 18px;
+//   font-weight: bold;
+//   text-transform: uppercase;
+//   border-radius: 50%;
+//   background-color: ${props => props.color};
+//   position: absolute;
+//   top: 200px;
+//   left: 28px;
+//   cursor: pointer;
+//   text-transform: uppercase;
+//   border: none;
+//   outline: none;
+//   ${media.small`
+//     display: inline;
+//   `}
+// `;
 
 const RightHeader = styled.div`
   display: flex;
@@ -78,43 +78,43 @@ const RightHeader = styled.div`
   ${media.small`
     z-index: 2;
     position: absolute;
-    top: 35px;
-    right: 37px;
+    top: 20px;
+    right: 30px;
     flex-flow: column nowrap;
   `}
 `;
 
-const Top = styled.div`
-  display: flex;
-  flex-flow: row-reverse nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  ${media.small`
-    flex-flow: row nowrap;
-    justify-content: flex-end;
-  `}
-`;
+// const Top = styled.div`
+//   display: flex;
+//   flex-flow: row-reverse nowrap;
+//   justify-content: flex-start;
+//   align-items: center;
+//   ${media.small`
+//     flex-flow: row nowrap;
+//     justify-content: flex-end;
+//   `}
+// `;
 
-const Color = styled.button`
-  height: 18px;
-  width: 18px;
-  border-radius: 50%;
-  margin-right: 12px;
-  background-color: ${param => param.color};
-  cursor: pointer;
-  text-transform: uppercase;
-  border: none;
-  outline: none;
-`;
+// const Color = styled.button`
+//   height: 18px;
+//   width: 18px;
+//   border-radius: 50%;
+//   margin-right: 12px;
+//   background-color: ${param => param.color};
+//   cursor: pointer;
+//   text-transform: uppercase;
+//   border: none;
+//   outline: none;
+// `;
 
-const Sale = styled(Label)`
-  margin-right: auto;
-  padding: 7px 20px 6px 21px;
-  ${media.small` 
-    margin-left: 17px;
-    margin-right: 0;
-  `}
-`;
+// const Sale = styled(Label)`
+//   margin-right: auto;
+//   padding: 7px 20px 6px 21px;
+// ${media.small`
+//     margin-left: 17px;
+//     margin-right: 0;
+//   `}
+// `;
 
 const Price = styled.h2`
   padding-bottom: 15px;
@@ -125,9 +125,6 @@ const Price = styled.h2`
   font-weight: bold;
   line-height: 1;
   color: ${props => props.color};
-  ${media.small`  
-    margin-top: 35px;
-  `}
 `;
 
 const colors = ['#c5c5c5', '#4d87ca', '#4a4a4a', '#e0e0e0'];
@@ -152,19 +149,19 @@ class Header extends Component {
       <Wrapper>
         <LeftHeader>
           <Name>{this.props.title}</Name>
-          <Save color={selectedColor}>Save</Save>
+          {/* <Save color={selectedColor}>Save</Save>*/}
         </LeftHeader>
         <RightHeader>
-          <Top>
-            {colors.map((color, index) => (
-              <Color
-                key={color}
-                color={color}
-                onClick={() => this.handleColorChange(index)}
-              />
-            ))}
-            <Sale>Sale</Sale>
-          </Top>
+          {/* <Top>*/}
+          {/* {colors.map((color, index) => (*/}
+          {/* <Color*/}
+          {/* key={color}*/}
+          {/* color={color}*/}
+          {/* onClick={() => this.handleColorChange(index)}*/}
+          {/* />*/}
+          {/* ))}*/}
+          {/* <Sale>Sale</Sale>*/}
+          {/* </Top>*/}
           <Price color={selectedColor}>${this.props.price}</Price>
         </RightHeader>
       </Wrapper>
